5c1b09b2fedfaf64dcdafb3fe73d13236e9a5c9b
oswin902m <1665434994@qq.com>
2026-05-18 22:58:37 +0800

    useEffect; loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [advice, setAdvice] = useState("Advice ...");
+  const [isLoading, setIsLoading] = useState(false);
 
   async function getAdvice() {
+    setIsLoading(true);
+
     // https://api.adviceslip.com/
     const response = await fetch("https://api.adviceslip.com/advice");
     const data = await response.json();
 
     setAdvice(data.slip.advice);
+    setIsLoading(false);
   }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
 
   return (
     <main>
       <h1>Advice App</h1>
-      <p>{advice}</p>
-      <button onClick={getAdvice}>Get Advice</button>
+      <p>{isLoading ? "Loading..." : advice}</p>
+      <button onClick={getAdvice} disabled={isLoading}>
+        Get Advice
+      </button>
     </main>
   );
 }
