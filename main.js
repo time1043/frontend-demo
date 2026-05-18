bfee411b0c45d1d1a3ccaed619ac4fd633791d2c
oswin902m <1665434994@qq.com>
2026-05-18 17:05:09 +0800

    login form (state and event)

--- main.js
+++ main.js
@@ -1,15 +1,47 @@
-function Header() {
-  return <h2>Welcome to My React App</h2>;
-}
-
 function App() {
-  // React allow only return one element
-  // Otherwise it needs to wrap it into a fragment
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`username: ${username}, password: ${password}`);
+
+    setUsername("");
+    setPassword("");
+
+    // const formData = new FormData(event.target);
+    // const username = formData.get("username")
+    // const password = formData.get("password")
+  }
+
   return (
-    <>
-      <Header />
-      <p>Welcome fellas..</p>
-    </>
+    <main>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        {/* input.id binding with label.for */}
+        {/* input.name is used for form data */}
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </main>
   );
 }
 
