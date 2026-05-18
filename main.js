ccf2c02623166b243c656366b0fe21241a6aa916
oswin902m <1665434994@qq.com>
2026-05-18 17:24:41 +0800

    list rendering, conditional rendering - react's is similar to js

--- main.js
+++ main.js
@@ -1,15 +1,88 @@
-function Header() {
-  return <h2>Welcome to My React App</h2>;
-}
+const cities = [
+  {
+    name: "New York",
+    country: "USA",
+    forecasts: [
+      { date: "2024-04-03", temperature: 15, weather: "Partly cloudy" },
+      { date: "2024-04-04", temperature: 17, weather: "Sunny" },
+      { date: "2024-04-05", temperature: 18, weather: "Partly cloudy" },
+      { date: "2024-04-06", temperature: 20, weather: "Rain" },
+      { date: "2024-04-07", temperature: 16, weather: "Thunderstorms" },
+      { date: "2024-04-08", temperature: 14, weather: "Cloudy" },
+      { date: "2024-04-09", temperature: 13, weather: "Partly cloudy" },
+    ],
+  },
+  {
+    name: "London",
+    country: "UK",
+    forecasts: [
+      { date: "2024-04-03", temperature: 12, weather: "Cloudy" },
+      { date: "2024-04-04", temperature: 14, weather: "Rain" },
+      { date: "2024-04-05", temperature: 15, weather: "Partly cloudy" },
+      { date: "2024-04-06", temperature: 13, weather: "Sunny" },
+      { date: "2024-04-07", temperature: 11, weather: "Cloudy" },
+      { date: "2024-04-08", temperature: 10, weather: "Rain" },
+      { date: "2024-04-09", temperature: 12, weather: "Partly cloudy" },
+    ],
+  },
+  {
+    name: "Tokyo",
+    country: "Japan",
+    forecasts: [
+      { date: "2024-04-03", temperature: 20, weather: "Sunny" },
+      { date: "2024-04-04", temperature: 21, weather: "Partly cloudy" },
+      { date: "2024-04-05", temperature: 22, weather: "Cloudy" },
+      { date: "2024-04-06", temperature: 19, weather: "Rain" },
+      { date: "2024-04-07", temperature: 18, weather: "Partly cloudy" },
+      { date: "2024-04-08", temperature: 17, weather: "Sunny" },
+      { date: "2024-04-09", temperature: 20, weather: "Cloudy" },
+    ],
+  },
+
+  {
+    name: "Sydney",
+    country: "Australia",
+    forecasts: [],
+  },
+
+  {
+    name: "Beijing",
+    country: "China",
+  },
+];
 
 function App() {
-  // React allow only return one element
-  // Otherwise it needs to wrap it into a fragment
+  function isForecastsValid(forecasts) {
+    return forecasts && forecasts.length > 0;
+  }
+
+  function renderForecastList(forecasts) {
+    return forecasts?.map((forecast) => (
+      <ul key={forecast.date}>
+        <li>
+          {forecast.date}
+          <span>
+            temperature: {forecast.temperature}℃({forecast.weather})
+          </span>
+        </li>
+      </ul>
+    ));
+  }
+
   return (
-    <>
-      <Header />
-      <p>Welcome fellas..</p>
-    </>
+    <main>
+      {cities.map((city) => (
+        <section className="city" key={city.name}>
+          <h2>{city.country}</h2>
+          <h3>{city.name}</h3>
+
+          {/* isForecastsValid(city.forecasts) ? renderForecastList(city.forecasts) : <p>Can't find any data</p> */}
+          {isForecastsValid(city.forecasts) &&
+            renderForecastList(city.forecasts)}
+          {!isForecastsValid(city.forecasts) && <p>Can't find any data</p>}
+        </section>
+      ))}
+    </main>
   );
 }
 
