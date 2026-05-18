78496493ed73ea376a906fcb4d6b1164c8a6ff05
oswin902m <1665434994@qq.com>
2026-05-18 23:08:01 +0800

    time display (useEffect cleanup)

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,22 @@ export default function App() {
     getAdvice();
   }, []);
 
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
+
+  useEffect(() => {
+    // setup code
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleString());
+    }, 1000);
+
+    // cleanup code
+    return () => clearInterval(interval);
+  }, []); // dependency array
+
   return (
     <main>
       <h1>Advice App</h1>
+      <span>{currentTime}</span>
       <p>{isLoading ? "Loading..." : advice}</p>
       <button onClick={getAdvice} disabled={isLoading}>
         Get Advice
