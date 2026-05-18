93827ec3511fbcb4e46b62e7648ef7ec28dc1e67
oswin902m <1665434994@qq.com>
2026-05-18 22:05:33 +0800

    migrate to vite from cdn

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,60 @@
+import { useState } from "react";
+
 export default function App() {
-  return <div>App</div>;
+  const [submitted, setSubmitted] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isUsernameValid = username.length >= 3;
+  const isPasswordValid = password.length >= 6;
+
+  const usernameClass = isUsernameValid || !submitted ? "" : "input-error";
+  const passwordClass = isPasswordValid || !submitted ? "" : "input-error";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSubmitted(true);
+
+    // validation
+    if (!isUsernameValid || !isPasswordValid)
+      return alert("Invalid username or password");
+
+    // mock submission
+    alert(`username: ${username}, password: ${password}`);
+
+    // reset
+    setUsername("");
+    setPassword("");
+    setSubmitted(false);
+  }
+
+  return (
+    <main>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          className={usernameClass}
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          className={passwordClass}
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 }
